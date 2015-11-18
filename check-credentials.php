f49e27a8ecea46ae62e8016204bd7b316be15fff
Knight125 <knight2@hawaii.edu>
2015-11-17 22:12:22 -1000

    First Commit

---
+++ check-credentials.php
@@ -0,0 +1,25 @@
+   <?php
+    $con=mysqli_connect("127.0.0.1", "root", "", "pricesite");
+    if (mysqli_connect_errno($con))
+    {
+        echo "MySql Error: " . mysqli_connect_error();
+        }
+
+    $query=mysqli_query($con,"SELECT * FROM user WHERE username='$_POST[username]' && password='$_POST[password]'");
+    $count=mysqli_num_rows($query);
+    $row=mysqli_fetch_array($query);
+
+    if ($count==1)
+    {
+        session_start();
+        $_SESSION['username'] = $_POST['username'];
+        $_SESSION['password'] = $_POST['password'];
+        header("location: login_success.php");
+    }
+    else
+    {
+        echo "Invalid username or password";
+    }
+
+    mysqli_close($con);
+    ?>
